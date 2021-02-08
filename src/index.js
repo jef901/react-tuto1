a7a731ccd4e06544bc70e2449f0520dd26cdfee5
Jose Freire <jef901@gmail.com>
2021-02-08 20:53:07 -0300

    08-02-21

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+// import css
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const books = [
+{
+  id: 1,
+  img:
+   'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+ },
+ {
+  id: 2,
+  img:
+   'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+  title: 'Our Class is a Family',
+  author: 'Shannon Olsen',
+ },
+ {
+  id: 3,
+  img:
+   'https://images-na.ssl-images-amazon.com/images/I/71e5m7xQd0L._AC_UL200_SR200,200_.jpg',
+  title: 'The Vanishing Half: A Novel',
+  author: 'Brit Bennett',
+ },
+];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ 
+function BookList(){
+  return (
+  <section className="booklist">
+     {books.map((book,index)=>{
+     return  <Book key={book.id} {...book}></Book>
+   })}
+  </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book =({img,title,author})=>{
+ // const { img,title,author } =props;
+const clickHandler = (e) => {
+   console.log(e);
+   console.log(e.target);
+   alert('hello world');
+}
+const complexExample = (author) => {
+   alert(author);
+
+}
+ return (
+    <article className="book" onMouseOver={()=>{
+       console.log(title)
+    } }>
+       <img src={img} alt=""/>
+       <h1 onClick={()=>console.log(title)}>{title}</h1>
+       <h4>{author}</h4>
+       <button type="button" onClick={clickHandler}>boton</button>
+       <button type="button" onClick={()=>complexExample(author)}>more complex example</button>
+    </article>
+   )
+}
+
+ReactDom.render(<BookList/>,document.getElementById('root'));
